d84132654218aeb897d41befe6bf72ae53f51e6b
EvelinMach <rosmerimach345@gmail.com>
2023-07-07 01:50:28 -0600

    funciones para crear y ver todos los profesores

--- Poomenu2/src/poomenu2/Poomenu2.java
+++ Poomenu2/src/poomenu2/Poomenu2.java
@@ -3,7 +3,8 @@ package poomenu2;
 import java.util.Scanner;
 
 public class Poomenu2 {
-
+ static profesor misMaestros[]=new profesor [10];
+  static int contador=0;
     public static void main(String[] args) {
         int op =0;
         Scanner entrada= new Scanner(System.in);
@@ -16,9 +17,18 @@ public class Poomenu2 {
             System.out.println("Ingrese el numero de opcion");
             op = entrada.nextInt();
             if(op==1){
+                Scanner teclado=new Scanner(System.in);
                 System.out.println("-----------Nuevo profesor--------------");
+                System.out.println("Ingrese el nombre del profesor");
+                String nombre= teclado.nextLine();
+                System.out.println("Ingrese la especialidad del profesor");
+                String materia=teclado.nextLine();
+                misMaestros[contador]=new profesor(nombre,materia);
+                System.out.println("Dato almacenado");
+                contador++;
             }else if(op ==2){
                 System.out.println("-----------Los datos son---------------");
+                verTodos();
             }else if(op ==3){
                 System.out.println("Programa finaliado");
             }else{
@@ -26,5 +36,11 @@ public class Poomenu2 {
             }
         }
     }
-    
+   static public void verTodos() {
+       for(int i=0;i<contador;i++ ){
+           System.out.println("Nombre: " + misMaestros[i].verNombre());
+           System.out.println("Materia " + misMaestros[i].verMateria());
+           System.out.println("##########################################");
+       }
+   }
 }
