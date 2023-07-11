3b7e4fda6c03ac7748f1a8a1f7991c40c173496d
EvelinMach <rosmerimach345@gmail.com>
2023-07-10 22:04:57 -0600

    crear profesor

--- Poomenu2/src/poomenu2/Poomenu2.java
+++ Poomenu2/src/poomenu2/Poomenu2.java
@@ -1,14 +1,16 @@
-
 package poomenu2;
+
 import java.util.Scanner;
 
 public class Poomenu2 {
- static profesor misMaestros[]=new profesor [10];
-  static int contador=0;
+
+    static profesor misMaestros[] = new profesor[10];
+    static int contador = 0;
+
     public static void main(String[] args) {
-        int op =0;
-        Scanner entrada= new Scanner(System.in);
-        while(op!=4){
+        int op = 0;
+        Scanner entrada = new Scanner(System.in);
+        while (op != 4) {
             System.out.println("----------------MENU----------------------");
             System.out.println("1. Crear profesor");
             System.out.println("2. Ver profesores");
@@ -16,31 +18,41 @@ public class Poomenu2 {
             System.out.println("4. Salir");
             System.out.println("Ingrese el numero de opcion");
             op = entrada.nextInt();
-            if(op==1){
-                Scanner teclado=new Scanner(System.in);
-                System.out.println("-----------Nuevo profesor--------------");
-                System.out.println("Ingrese el nombre del profesor");
-                String nombre= teclado.nextLine();
-                System.out.println("Ingrese la especialidad del profesor");
-                String materia=teclado.nextLine();
-                misMaestros[contador]=new profesor(nombre,materia);
-                System.out.println("Dato almacenado");
-                contador++;
-            }else if(op ==2){
+            if (op == 1) {
+                crearProfesor();
+            } else if (op == 2) {
                 System.out.println("-----------Los datos son---------------");
                 verTodos();
-            }else if(op ==3){
+            } else if (op == 3) {
                 System.out.println("Programa finaliado");
-            }else{
+            } else {
                 System.out.println("Opcion incorrecta");
             }
         }
     }
-   static public void verTodos() {
-       for(int i=0;i<contador;i++ ){
-           System.out.println("Nombre: " + misMaestros[i].verNombre());
-           System.out.println("Materia " + misMaestros[i].verMateria());
-           System.out.println("##########################################");
-       }
-   }
+
+    static public void crearProfesor() {
+        if (contador >= 10) {
+            System.out.println("la memoria esta llena");
+        } else {
+            Scanner teclado = new Scanner(System.in);
+            System.out.println("-----------Nuevo profesor--------------");
+            System.out.println("Ingrese el nombre del profesor");
+            String nombre = teclado.nextLine();
+            System.out.println("Ingrese la especialidad del profesor");
+            String materia = teclado.nextLine();
+            misMaestros[contador] = new profesor(nombre, materia);
+            System.out.println("Dato almacenado");
+            contador++;
+
+        }
+    }
+
+    static public void verTodos() {
+        for (int i = 0; i < contador; i++) {
+            System.out.println("Nombre: " + misMaestros[i].verNombre());
+            System.out.println("Materia " + misMaestros[i].verMateria());
+            System.out.println("##########################################");
+        }
+    }
 }
